618377b8b02b415d5d19b264bf9660bb774041ce
21341A05F1_R Naresh Reddy <rangalanareshreddy7406@gmail.com>
2024-06-27 11:19:58 +0530

    type_conv_in_If_condition.c

---
+++ type_conv_in_If_condition.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int x=29;
+  float y = 29.0;
+  if(x==y){
+    printf("both are equal");
+  }
+  else{
+     printf("both are not equal");
+  }
+    return 0;
+}
