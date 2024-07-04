ebd494652398fe2d85c6744a36340767290bcd90
21341A05F1_R Naresh Reddy <rangalanareshreddy7406@gmail.com>
2024-07-04 11:08:20 +0530

    arry_init_less_vlaues.c

---
+++ arry_init_less_vlaues.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main(){
+  int arr[5] = {1,2}; \\ reamining indexces are implicitly initialzed with zero for nmneric and '\0' null character equivalent to zero is used for char array
+}
