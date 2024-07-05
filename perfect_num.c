96fce0d98133df2412b0f9f5ebc162fe3a1578c3
21341A05F1_R Naresh Reddy <rangalanareshreddy7406@gmail.com>
2024-07-05 09:55:15 +0530

    perfect_num.c

---
+++ perfect_num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num,n,sum=0;
+    printf("enter the number:");
+    scanf("%d",&num);
+    for(int i=2;i<num;i++){
+        if(num%i==0) sum = sum + i;
+    }
+    printf(num==sum+1?"yes":"No");
+    return 0;
+}
